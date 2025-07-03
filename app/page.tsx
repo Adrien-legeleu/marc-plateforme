4bf6f737b7ece03b5dad3382c91d2a1d35f3143b
Adrien Legeleux <adrienlegeleux@MacBook-Air-de-Adrien.local>
2025-07-04 01:35:04 +0200

    marc

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <div className="grid grid-cols-3 h-[120px] sm:gap-10 gap-5 z-10 w-full max-w-4xl">
           <MagneticButton>
             <Link
-              className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
+              className="bg-gradient-to-b items-center justify-center from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
               href={'/profils'}
             >
               <IconUser className="sm:min-w-8 sm:min-h-8 min-w-6 min-h-6" />
@@ -31,7 +31,7 @@ export default function Home() {
 
           <MagneticButton>
             <Link
-              className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
+              className="bg-gradient-to-b from-marcblue items-center justify-center to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
               href={'/profils'}
             >
               {' '}
@@ -42,7 +42,7 @@ export default function Home() {
 
           <MagneticButton>
             <Link
-              className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
+              className="bg-gradient-to-b from-marcblue items-center justify-center to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
               href={'/profils'}
             >
               {' '}
