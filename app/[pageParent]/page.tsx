53fbf863a1a9096fe2705ddceda0d94895cffb35
Adrien Legeleux <adrienlegeleux@MacBook-Air-de-Adrien.local>
2025-07-04 00:15:33 +0200

    marc

--- app/[pageParent]/page.tsx
+++ app/[pageParent]/page.tsx
@@ -1,23 +1,19 @@
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
 import { prisma } from '@/lib/prisma';
-import type { Article } from '@prisma/client';
 
-export default async function Page({
-  params,
-}: {
-  params: { pageParent: string };
+export default async function Page(props: {
+  params: Promise<{ pageParent: string; slug: string }>;
 }) {
-  const articles: Article[] = await prisma.article.findMany({
-    where: { pageParent: params.pageParent },
-  });
+  const { pageParent, slug } = await props.params;
 
-  if (!articles.length) return notFound();
+  const articles = await prisma.article.findMany({ where: { pageParent } });
+  if (articles.length === 0) return notFound();
 
   return (
     <div className="py-40 max-w-5xl mx-auto px-5">
       <h1 className="text-5xl text-center mb-20 font-bold capitalize">
-        {params.pageParent}
+        {pageParent}
       </h1>
       <ul className="space-y-6 flex flex-wrap items-center justify-center gap-10">
         {articles.map((article) => (
