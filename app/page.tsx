8af72d382779ec4b3fdcc60ddaa46315c927df35
Adrien Legeleux <adrienlegeleux@MacBook-Air-de-Adrien.local>
2025-07-04 01:04:25 +0200

    marc

--- app/page.tsx
+++ app/page.tsx
@@ -20,24 +20,35 @@ export default function Home() {
 
         <div className="grid grid-cols-3 h-[120px] sm:gap-10 gap-5 z-10 w-full max-w-4xl">
           <MagneticButton>
-            <Button className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]">
+            <Link
+              className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
+              href={'/profils'}
+            >
               <IconUser className="sm:min-w-8 sm:min-h-8 min-w-6 min-h-6" />
               Joueur
-            </Button>
+            </Link>
           </MagneticButton>
 
           <MagneticButton>
-            <Button className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]">
+            <Link
+              className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
+              href={'/profils'}
+            >
+              {' '}
               <IconUsersGroup className="sm:min-w-8 sm:min-h-8 min-w-6 min-h-6" />
               Club
-            </Button>
+            </Link>
           </MagneticButton>
 
           <MagneticButton>
-            <Button className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]">
+            <Link
+              className="bg-gradient-to-b from-marcblue to-marcblue/40 flex flex-col gap-3 font-bold shadow-xl backdrop-blur-sm w-full h-full hover:bg-blue-600/60 cursor-pointer transition-colors sm:text-xl text-white sm:py-5 py-2 rounded-[3rem]"
+              href={'/profils'}
+            >
+              {' '}
               <IconClipboardList className="sm:min-w-8 sm:min-h-8 min-w-6 min-h-6" />
               Entraîneur
-            </Button>
+            </Link>
           </MagneticButton>
         </div>
         <Button
